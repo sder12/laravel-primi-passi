601ca09105e9640e01347a432da5b9f4aeebd408
sder12 <sara.cetto@gmail.com>
2022-12-14 16:58:18 +0100

    Adding hompage blade

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header class="my-2 text-center">
+            <h4>This is the header</h4>
+        </header>
+        <main class="py-3">
+            <h2>This is the main</h2>
+        </main>
+        <footer class="my-2 text-center">
+            <h4>This is the footer</h4>
+        </footer>
+    </div>
+</body>
+</html>
